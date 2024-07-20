d380b3fd919c8f4de83784e4c3e74b68842e3b5f
ANDJO <13768626+yan_zhuo@user.noreply.gitee.com>
2024-07-20 18:04:47 +0800

    usart通上位机

---
+++ usart1/MDK-ARM/RTE/_usart1/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'usart1' 
+ * Target:  'usart1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+#endif /* RTE_COMPONENTS_H */
